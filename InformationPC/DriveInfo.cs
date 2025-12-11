4fd38cc9f64c629cc6c7100a9868e35b973a19fb
4090rt <artem2007yannurow@gmail.com>
2025-12-11 21:38:23 +0500

    Add files via upload

---
+++ InformationPC/DriveInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryStatistic.InformationPC
+{
+    public class DriveInfo
+    {
+        public void DRIVEIBFO()
+        {
+            try
+            {
+                using var information = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+                {
+                    foreach (var item in information.Get())
+                    {
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine($"\nСистемный Диск:  {item["Model"]}");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        var pam = Convert.ToUInt64(item["Size"]) / (1024 * 1024 * 1024);
+                        Console.WriteLine($"Память GB: {pam}");
+                        Console.WriteLine($"Разделов:  {item["Partitions"]}");
+                        Console.WriteLine($"Тип интерфейса:  {item["InterfaceType"]}");
+                        Console.WriteLine($"Статус:  {item["Status"]}"); ;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Не удалось получить информацию о диске" + ex.Message);
+            }
+        }
+    }
+}
---
+++ InformationPC/MotherBoardInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryStatistic.InformationPC
+{
+    public class MotherBoardInfo
+    {
+        public void MotherBoardInfos()
+        {
+            try
+            {
+                using var informatio = new ManagementObjectSearcher("SELECT * FROM Win32_BaseBoard");
+                {
+                    foreach (var item in informatio.Get())
+                    {
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine($"\nМатеринская плата:  {item["Product"]}");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine($"Мануфактура:  {item["Manufacturer"]}");
+                        Console.WriteLine($"Имя:  {item["Name"]}");
+                        Console.WriteLine($"Статус:  {item["Status"]}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Не удалось получить информацию о материнской плате" + ex.Message);
+            }
+        }
+    }
+}
