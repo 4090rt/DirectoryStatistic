8d9c025aac01d90ecf9eae603f7bbb2f85d57c6f
4090rt <artem2007yannurow@gmail.com>
2026-04-02 18:32:48 +0300

    Add files via upload

---
+++ src/PCA/Http/ModelData/DataResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DirectoryStatistic.Http.ModelData
+{
+    public class DataResult
+    {
+        public string Host { get; set; } = string.Empty;
+        
+        [JsonIgnore]
+        public System.Net.IPAddress[] Addresses { get; set; } = Array.Empty<System.Net.IPAddress>();
+        
+        /// <summary>
+        /// Список IPv4 адресов
+        /// </summary>
+        [JsonPropertyName("ipv4")]
+        public string[] IPv4 { get; set; } = Array.Empty<string>();
+        
+        /// <summary>
+        /// Список IPv6 адресов
+        /// </summary>
+        [JsonPropertyName("ipv6")]
+        public string[] IPv6 { get; set; } = Array.Empty<string>();
+        
+        public long ResolveTime { get; set; }
+        public bool Success { get; set; }
+        public string? Error { get; set; }
+    }
+}
